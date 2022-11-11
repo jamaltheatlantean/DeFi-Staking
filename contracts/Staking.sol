81404e582919279c5902a916c8334949c553f4cd
Gabriel Isobara <jamaltheatlantean@gmail.com>
2022-11-11 21:29:19 +0100

    Update Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -93,7 +93,6 @@ contract Staking {
             revert Staking__TransferFailed();
         }
         //
-        // sends tokens to all tokenStakers
         //
         // staked tokens: 50 staked tokens, 30 staked tokens, 20 staked tokens
         // reward: 50 staked tokens, 30 staked tokens, 20 staked tokens
