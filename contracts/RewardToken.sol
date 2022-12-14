e78011e9bdacca696f88feaf1aa9b7a4067993ed
Gabriel Isobara <jamaltheatlantean@gmail.com>
2022-12-14 20:53:18 +0100

    Update RewardToken.sol

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// Reward users ATN tokens for their stake
 contract RewardToken is ERC20 {
     constructor() ERC20("AtlanteanNotes", "ATN") {
         _mint(msg.sender, 1000000 * 10**18);
